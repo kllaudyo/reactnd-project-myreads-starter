72ef4a5b32a6728feceab492149fe72b00ac9dd5
Claudio Acioli <kllaudyo@gmail.com>
2017-11-11 00:06:30 -0200

    refactor: Implementa biblioteca PropTypes

--- src/Book.js
+++ src/Book.js
@@ -1,50 +1,47 @@
-import React from 'react'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types';
 
-/**
- * Esta função de callback chamada `onChangeShelf` é usada para alterar a estante atual do livro
- * @callback onChangeShelf
- * @param {string} id - atributo identificador único do livro a ser alterado
- * @param {string} shelf - estante ao qual o livro será atribuído
- */
+/** Class Representa o componente Book. */
+class Book extends Component{
 
-/**
- * @description Representa o componente Book
- * @param {Object} props - Os atributos do componente
- * @param {Object} props.book - O objeto book que descreve um livro
- * @param {onChangeShelf} props.onChangeShelf - O callback que trata estante do livro
- * @returns {Object}
- */
-function Book(props){
+  static propTypes = {
+    book : PropTypes.object.isRequired,
+    onChangeShelf: PropTypes.func.isRequired
+  };
 
-  const {book, onChangeShelf} = props;
-  const {title, authors, shelf, imageLinks:cover} = book;
+  render(){
 
-  return (
-    <div className="book">
-      <div className="book-top">
-        <div className="book-cover" style={{ width: 128, height: 192, backgroundImage: `url(${cover.smallThumbnail})` }}></div>
-        <div className="book-shelf-changer">
-          <select defaultValue={shelf} onChange={(event) => {
-            onChangeShelf(book, event.target.value);
-          }}>
-            <option value="none" disabled>Move to...</option>
-            <option value="currentlyReading">Currently Reading</option>
-            <option value="wantToRead">Want to Read</option>
-            <option value="read">Read</option>
-            <option value="none">None</option>
-          </select>
+    const {book, onChangeShelf} = this.props;
+    const {title, authors, shelf, imageLinks:cover} = book;
+
+    return (
+      <div className="book">
+        <div className="book-top">
+          <div className="book-cover"
+               style={{width: 128, height: 192, backgroundImage: `url(${cover.smallThumbnail})`}}></div>
+          <div className="book-shelf-changer">
+            <select defaultValue={shelf} onChange={(event) => {
+              onChangeShelf(book, event.target.value);
+            }}>
+              <option value="none" disabled>Move to...</option>
+              <option value="currentlyReading">Currently Reading</option>
+              <option value="wantToRead">Want to Read</option>
+              <option value="read">Read</option>
+              <option value="none">None</option>
+            </select>
+          </div>
+        </div>
+        <div className="book-title">{title}</div>
+        <div className="book-authors">
+          <ol className="authors-list">
+            {authors.map((author, index) => (
+              <li key={index}>{author}</li>
+            ))}
+          </ol>
         </div>
       </div>
-      <div className="book-title">{title}</div>
-      <div className="book-authors">
-        <ol className="authors-list">
-          {authors.map( (author,index) => (
-            <li key={index}>{author}</li>
-          ))}
-        </ol>
-      </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default Book;
