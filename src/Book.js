cb905385a9f182e9cb57ab3a0589ce8d5b8400e7
Claudio Acioli <kllaudyo@gmail.com>
2017-11-15 15:25:02 -0200

    refactor: Separa componente Book

--- src/Book.js
+++ src/Book.js
@@ -24,6 +24,40 @@ class Book extends Component{
       });
   }
 
+  static renderTitle(title) {
+    return <div className="book-title">{title}</div>
+  };
+
+  static renderCover(cover) {
+    return <div className="book-cover" style={{width: 128, height: 192, backgroundImage: `url(${cover})`}}/>;
+  };
+
+  static renderAuthors(authors) {
+    return (
+      <div className="book-authors">
+        <ol className="authors-list">
+          {authors.map((author, index) => (
+            <li key={index}>{author}</li>
+          ))}
+        </ol>
+      </div>
+    );
+  }
+
+  static renderShelf(shelf, onChange) {
+    return (
+      <div className="book-shelf-changer">
+        <select value={shelf} onChange={event=>onChange(event.target.value)}>
+          <option value="moveTo" disabled>Move to...</option>
+          <option value="currentlyReading">Currently Reading</option>
+          <option value="wantToRead">Want to Read</option>
+          <option value="read">Read</option>
+          <option value="none">None</option>
+        </select>
+      </div>
+    );
+  }
+
   render() {
 
     const {onChangeShelf} = this.props;
@@ -33,28 +67,11 @@ class Book extends Component{
     return (
       <div className="book">
         <div className="book-top">
-          <div className="book-cover"
-               style={{width: 128, height: 192, backgroundImage: `url(${cover?cover.smallThumbnail:''})`}}></div>
-          <div className="book-shelf-changer">
-            <select value={shelf} onChange={(event) => {
-              onChangeShelf(book, event.target.value);
-            }}>
-              <option value="none" disabled>Move to...</option>
-              <option value="currentlyReading">Currently Reading</option>
-              <option value="wantToRead">Want to Read</option>
-              <option value="read">Read</option>
-              <option value="none">None</option>
-            </select>
-          </div>
+          {Book.renderCover(cover?cover.smallThumbnail:'')}
+          {Book.renderShelf(shelf, shelf => onChangeShelf(book, shelf))}
         </div>
-        <div className="book-title">{title}</div>
-        <div className="book-authors">
-          <ol className="authors-list">
-            {authors.map((author, index) => (
-              <li key={index}>{author}</li>
-            ))}
-          </ol>
-        </div>
+        {Book.renderTitle(title)}
+        {Book.renderAuthors(authors)}
       </div>
     );
   }
