7cc9efe0ff6484d2473152b44d9accce0fcb6f38
Claudio Acioli <kllaudyo@gmail.com>
2017-11-16 21:22:01 -0200

    refactor: Converte Book para componente sem estado

--- src/Book.js
+++ src/Book.js
@@ -1,80 +1,52 @@
-import React, {Component} from 'react'
+import React from 'react'
 import PropTypes from 'prop-types';
-import {get} from './BooksAPI';
 
-/** Class Representa o componente Book. */
-class Book extends Component{
+const BookTitle = ({title}) =>
+  <div className="book-title">{title}</div>;
 
-  state = {
-    book : this.props.book
-  };
+const BookCover = ({cover}) =>
+  <div className="book-cover" style={{width: 128, height: 192, backgroundImage: `url(${cover})`}}/>;
 
-  static propTypes = {
-    book: PropTypes.object.isRequired,
-    id: PropTypes.string.isRequired,
-    onChangeShelf: PropTypes.func.isRequired
-  };
+const BookAuthors = ({authors}) =>
+  <div className="book-authors">
+    <ol className="authors-list">
+      {authors.map((author, index) => (
+        <li key={index}>{author}</li>
+      ))}
+    </ol>
+  </div>;
 
-  componentDidMount(){
-    get(this.props.id)
-      .then(book => this.setState({book:book}))
-      .catch(response => {
-        console.error(response);
-        this.setState({book:null})
-      });
-  }
+const BookShelf = ({shelf, onChange}) =>
+  <div className="book-shelf-changer">
+    <select value={shelf} onChange={event=>onChange(event.target.value)}>
+      <option value="moveTo" disabled>Move to...</option>
+      <option value="currentlyReading">Currently Reading</option>
+      <option value="wantToRead">Want to Read</option>
+      <option value="read">Read</option>
+      <option value="none">None</option>
+    </select>
+  </div>;
 
-  static renderTitle(title) {
-    return <div className="book-title">{title}</div>
-  };
-
-  static renderCover(cover) {
-    return <div className="book-cover" style={{width: 128, height: 192, backgroundImage: `url(${cover})`}}/>;
-  };
-
-  static renderAuthors(authors) {
-    return (
-      <div className="book-authors">
-        <ol className="authors-list">
-          {authors.map((author, index) => (
-            <li key={index}>{author}</li>
-          ))}
-        </ol>
+const Book = ({id, title, authors = [], shelf, cover, onChangeShelf}) => {
+  return (
+    <div className="book">
+      <div className="book-top">
+        <BookCover cover={cover} />
+        <BookShelf shelf={shelf} onChange={(shelf)=>onChangeShelf(id, shelf)} />
       </div>
-    );
-  }
+      <BookTitle title={title} />
+      <BookAuthors authors={authors} />
+    </div>
+  )
+};
 
-  static renderShelf(shelf, onChange) {
-    return (
-      <div className="book-shelf-changer">
-        <select value={shelf} onChange={event=>onChange(event.target.value)}>
-          <option value="moveTo" disabled>Move to...</option>
-          <option value="currentlyReading">Currently Reading</option>
-          <option value="wantToRead">Want to Read</option>
-          <option value="read">Read</option>
-          <option value="none">None</option>
-        </select>
-      </div>
-    );
-  }
-
-  render() {
-
-    const {onChangeShelf} = this.props;
-    const {book} = this.state;
-    const {title = "", authors = [], shelf = "none", imageLinks: cover} = book;
-
-    return (
-      <div className="book">
-        <div className="book-top">
-          {Book.renderCover(cover?cover.smallThumbnail:'')}
-          {Book.renderShelf(shelf, shelf => onChangeShelf(book, shelf))}
-        </div>
-        {Book.renderTitle(title)}
-        {Book.renderAuthors(authors)}
-      </div>
-    );
-  }
-}
+Book.propTypes = {
+  id: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  cover: PropTypes.string.isRequired,
+  authors: PropTypes.array,
+  shelf: PropTypes.string,
+  onChangeShelf: PropTypes.func.isRequired
+};
 
 export default Book;
