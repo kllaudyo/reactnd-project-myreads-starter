3c1a6386af66621e783f3d3dd249ddcf1a71076a
Claudio Acioli <kllaudyo@gmail.com>
2017-11-18 13:13:20 -0200

    refactor: Torna código menos verboso

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Route, Link} from 'react-router-dom'
 import Book from './Book'
 import Search from './Search'
-import * as BooksAPI from './BooksAPI'
+import {get, getAll, update} from "./BooksAPI"
 import './App.css'
 
 class BooksApp extends React.Component {
@@ -10,38 +10,37 @@ class BooksApp extends React.Component {
   constructor(){
     super();
     this.state = {
-      shelfBooks: []
+      books: []
     };
 
-    this.updateShelf = this.updateShelf.bind(this);
+    this.setShelf = this.setShelf.bind(this);
     this.getShelf = this.getShelf.bind(this);
-    this.updateBookAPI = this.updateBookAPI.bind(this);
+    this.updateAPI = this.updateAPI.bind(this);
   }
 
-  updateBookAPI(book, shelfBooks){
-    BooksAPI
-      .update(book, book.shelf)
-      .then(response=> this.setState({shelfBooks}))
+  updateAPI(book, books){
+    update(book, book.shelf)
+      .then(response=> this.setState({books}))
       .catch(error => console.log(error));
   }
 
-  updateShelf(id, shelf) {
-    const shelfBooks = this.state.shelfBooks.filter(book => book.id !== id);
-    BooksAPI.get(id).then(book=>{
+  setShelf(id, shelf) {
+    const books = this.state.books.filter(book => book.id !== id);
+    get(id).then(book=>{
       book.shelf = shelf;
-      shelfBooks.push(book);
-      this.updateBookAPI(book, shelfBooks);
+      books.push(book);
+      this.updateAPI(book, books);
     });
   }
 
   getShelf(id) {
-    const books = this.state.shelfBooks.filter(book => book.id === id);
+    const books = this.state.books.filter(book => book.id === id);
     return books.length > 0 ? books[0].shelf:"none";
   }
 
   componentDidMount() {
-    BooksAPI.getAll().then(books => {
-      this.setState({shelfBooks:books})
+    getAll().then(books => {
+      this.setState({books})
     })
   }
 
@@ -51,7 +50,7 @@ class BooksApp extends React.Component {
       <div className="app">
 
         <Route path="/search" render={() => (
-          <Search onChangeShelf={this.updateShelf} getShelf={this.getShelf} />
+          <Search onChangeShelf={this.setShelf} getShelf={this.getShelf} />
         )} />
 
         <Route exact path="/" render={() => (
@@ -65,14 +64,14 @@ class BooksApp extends React.Component {
                   <h2 className="bookshelf-title">Currently Reading</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      {this.state.shelfBooks.filter( book => ( book.shelf === 'currentlyReading') ).map( book => (
+                      {this.state.books.filter( book => ( book.shelf === 'currentlyReading') ).map( book => (
                         <li key={book.id}>
                           <Book id={book.id}
                                 title={book.title}
                                 authors={book.authors}
                                 cover={book.imageLinks.smallThumbnail}
                                 shelf={book.shelf}
-                                onChangeShelf={this.updateShelf}
+                                onChangeShelf={this.setShelf}
                           />
                         </li>
                       ))}
@@ -84,14 +83,14 @@ class BooksApp extends React.Component {
                   <h2 className="bookshelf-title">Want to Read</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      {this.state.shelfBooks.filter( book => ( book.shelf === 'wantToRead') ).map( book => (
+                      {this.state.books.filter( book => ( book.shelf === 'wantToRead') ).map( book => (
                         <li key={book.id}>
                           <Book id={book.id}
                                 title={book.title}
                                 authors={book.authors}
                                 cover={book.imageLinks.smallThumbnail}
                                 shelf={book.shelf}
-                                onChangeShelf={this.updateShelf}
+                                onChangeShelf={this.setShelf}
                           />
                         </li>
                       ))}
@@ -103,14 +102,14 @@ class BooksApp extends React.Component {
                   <h2 className="bookshelf-title">Read</h2>
                   <div className="bookshelf-books">
                     <ol className="books-grid">
-                      {this.state.shelfBooks.filter( book => ( book.shelf === 'read') ).map( book => (
+                      {this.state.books.filter( book => ( book.shelf === 'read') ).map( book => (
                         <li key={book.id}>
                           <Book id={book.id}
                                 title={book.title}
                                 authors={book.authors}
                                 cover={book.imageLinks.smallThumbnail}
                                 shelf={book.shelf}
-                                onChangeShelf={this.updateShelf}
+                                onChangeShelf={this.setShelf}
                           />
                         </li>
                       ))}
